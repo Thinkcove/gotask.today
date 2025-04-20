be51b763aacfec632d607ebff0e163b7a671ab9a
Rizwana <rizwana@thinkcove.com>
2025-04-20 09:59:46 +0530

    build error fixes

--- apps/gotask_ui/src/app/portal/task/component/taskLayout/statusIndicator.tsx
+++ apps/gotask_ui/src/app/portal/task/component/taskLayout/statusIndicator.tsx
@@ -1,24 +1,17 @@
 import React from "react";
 import { Box, Typography, Divider } from "@mui/material";
 import { statuses } from "@/app/common/constants/task";
-import { fetchTaskStatusCounts } from "../service/taskAction";
 import useSWR from "swr";
+import { fetchTaskStatusCounts } from "../../service/taskAction";
 
 const StatusIndicator: React.FC = () => {
   const { data } = useSWR("fetch-task-status-counts", fetchTaskStatusCounts, {
-    revalidateOnFocus: false,
+    revalidateOnFocus: false
   });
   const statusCounts = data?.data || {};
 
   return (
-    <Box
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      gap={2}
-      p={2}
-      flexWrap="wrap"
-    >
+    <Box display="flex" justifyContent="center" alignItems="center" gap={2} p={2} flexWrap="wrap">
       {statuses.map((status, index) => (
         <React.Fragment key={status.label}>
           <Box display="flex" alignItems="center" gap={1}>
@@ -27,21 +20,15 @@ const StatusIndicator: React.FC = () => {
                 width: 15,
                 height: 15,
                 borderRadius: "50%",
-                backgroundColor: status.color,
+                backgroundColor: status.color
               }}
             />
             <Typography variant="body2">
-              {status.label}:{" "}
-              {statusCounts[status.label.toLowerCase().replace(/\s/g, "-")] ||
-                0}
+              {status.label}: {statusCounts[status.label.toLowerCase().replace(/\s/g, "-")] || 0}
             </Typography>
           </Box>
           {index < statuses.length - 1 && (
-            <Divider
-              orientation="vertical"
-              flexItem
-              sx={{ height: 18, mx: 1 }}
-            />
+            <Divider orientation="vertical" flexItem sx={{ height: 18, mx: 1 }} />
           )}
         </React.Fragment>
       ))}

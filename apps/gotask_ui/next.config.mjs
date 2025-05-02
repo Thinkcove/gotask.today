c2813bcc1fb4c90bed1eeb63ad712f1c589c83f8
snega <arrowxuidev@thinkcove.com>
2025-05-02 13:59:58 +0530

    comments updated

--- apps/gotask_ui/next.config.mjs
+++ apps/gotask_ui/next.config.mjs
@@ -1,12 +1,3 @@
-// /** @type {import('next').NextConfig} */
-// const NEXT_PUBLIC_API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
-// const nextConfig = {
-//   env: {
-//     NEXT_PUBLIC_API_BASE_URL: NEXT_PUBLIC_API_BASE_URL
-//   }
-// };
-// module.exports = nextConfig;
-
 /* eslint-env node */
 // Or if you prefer individual disables:
 /* eslint-disable no-undef */

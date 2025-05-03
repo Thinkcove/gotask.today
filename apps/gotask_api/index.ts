4e008b49662259b7c32d1c2b82bb671595258767
Rizwana <rizwana@thinkcove.com>
2025-05-03 17:42:54 +0530

    updates

--- apps/gotask_api/index.ts
+++ apps/gotask_api/index.ts
@@ -1,28 +1,38 @@
 import Hapi from "@hapi/hapi";
 import connectDB from "./src/config/database";
 import dotenv from "dotenv";
-
+import { plugins } from "./src/plugins/plugins";
 import routes from "./src/routes/route";
+import authStrategy from "./src/constants/utils.ts/authStrategy";
+import { authValidation } from "./src/constants/utils.ts/authValidation";
 
 dotenv.config();
-const init = async () => {
-  await connectDB();
-  const server = Hapi.server({
-    port: process.env.PORT,
-    host: "localhost",
-    routes: {
-      cors: {
-        origin: ["*"], // Allow all origins
-        headers: ["Accept", "Content-Type", "Authorization"], // Allowed headers
-        credentials: true // Allow credentials (e.g., cookies, authorization)
-      }
+const server = Hapi.server({
+  port: process.env.PORT,
+  host: "localhost",
+  routes: {
+    cors: {
+      origin: ["*"],
+      headers: ["Accept", "Content-Type", "Authorization"],
+      credentials: true
     }
-  });
+  }
+});
 
- 
-
+const start = async () => {
+  await server.register(plugins);
+  server.auth.strategy(
+    authStrategy.SIMPLE,
+    authStrategy.BEARERSCHEME,
+    authValidation.validateUserToken
+  );
   server.route(routes);
   await server.start();
+};
+
+const init = async () => {
+  await connectDB();
+  await start();
 };
 
 init();

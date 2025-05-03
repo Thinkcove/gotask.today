4c5d1109d65f67db53a9e1c340b077875d03994a
Rizwana <rizwana@thinkcove.com>
2025-05-03 14:20:47 +0530

    user routes checks

--- apps/gotask_api/src/modules/user/userRoutes.ts
+++ apps/gotask_api/src/modules/user/userRoutes.ts
@@ -3,18 +3,22 @@ import { API_PATHS } from "../../constants/api/apiPaths";
 import { API, API_METHODS } from "../../constants/api/apiMethods";
 import RequestHelper from "../../helpers/requestHelper";
 import UserController from "./userController";
+import { withAccessCheck } from "../../middleware/withAccessCheck";
+import { ACTIONS, APPLICATIONS } from "../../constants/utils.ts/access";
 
 const userController = new UserController();
-
 const tags = [API, "User"];
 const UserRoutes = [];
+
+const appName = APPLICATIONS.USER_MANAGEMENT;
 
 // Route: Create User
 UserRoutes.push({
   path: API_PATHS.CREATE_USER,
   method: API_METHODS.POST,
-  handler: (request: Request, handler: ResponseToolkit) =>
-    userController.createUser(new RequestHelper(request), handler),
+  handler: withAccessCheck(appName, ACTIONS.CREATE, (request: Request, handler: ResponseToolkit) =>
+    userController.createUser(new RequestHelper(request), handler)
+  ),
   config: {
     notes: "Create a new user",
     tags
@@ -25,8 +29,9 @@ UserRoutes.push({
 UserRoutes.push({
   path: API_PATHS.GET_USERS,
   method: API_METHODS.GET,
-  handler: (request: Request, handler: ResponseToolkit) =>
-    userController.getAllUsers(new RequestHelper(request), handler),
+  handler: withAccessCheck(appName, ACTIONS.LIST, (request: Request, handler: ResponseToolkit) =>
+    userController.getAllUsers(new RequestHelper(request), handler)
+  ),
   config: {
     notes: "Get all users",
     tags
@@ -37,8 +42,9 @@ UserRoutes.push({
 UserRoutes.push({
   path: API_PATHS.GET_USER_BY_ID,
   method: API_METHODS.GET,
-  handler: (request: Request, handler: ResponseToolkit) =>
-    userController.getUserById(new RequestHelper(request), handler),
+  handler: withAccessCheck(appName, ACTIONS.READ, (request: Request, handler: ResponseToolkit) =>
+    userController.getUserById(new RequestHelper(request), handler)
+  ),
   config: {
     notes: "Get a user by ID",
     tags
@@ -49,15 +55,16 @@ UserRoutes.push({
 UserRoutes.push({
   path: API_PATHS.UPDATE_USER,
   method: API_METHODS.PUT,
-  handler: (request: Request, handler: ResponseToolkit) =>
-    userController.updateUser(new RequestHelper(request), handler),
+  handler: withAccessCheck(appName, ACTIONS.UPDATE, (request: Request, handler: ResponseToolkit) =>
+    userController.updateUser(new RequestHelper(request), handler)
+  ),
   config: {
     notes: "Update user details",
     tags
   }
 });
 
-// Route: Login User
+// Route: Login User (Public – no access check)
 UserRoutes.push({
   path: API_PATHS.LOGIN,
   method: API_METHODS.POST,
@@ -73,8 +80,9 @@ UserRoutes.push({
 UserRoutes.push({
   path: API_PATHS.DELETE_USER,
   method: API_METHODS.DELETE,
-  handler: (request: Request, handler: ResponseToolkit) =>
-    userController.deleteUser(new RequestHelper(request), handler),
+  handler: withAccessCheck(appName, ACTIONS.DELETE, (request: Request, handler: ResponseToolkit) =>
+    userController.deleteUser(new RequestHelper(request), handler)
+  ),
   config: {
     notes: "Delete a user by ID",
     tags

a4acd23ae4fc23c9676912614e7949f5a48d782a
Rizwana <rizwana@thinkcove.com>
2025-06-17 22:40:04 +0530

    script updates

--- apps/gotask_ui/deploy-dev-ui.sh
+++ apps/gotask_ui/deploy-dev-ui.sh
@@ -1,31 +1,37 @@
-# Remove the previous build folder
-rm -rf .next
-
 keyfile="tc-test-key1.pem"
 user="ec2-user"
 ipaddr="13.127.153.17"
-appFolder="/home/ec2-user/gotask/test/app/"
+appFolder="/home/ec2-user/gotask/dev/app/"
 
-#Create new build 
-npm run build
-
-echo "Build created"
+#Create Build 
+bash dev-build.sh
 
 echo "Removing the Previous build"
+# Remove the previous build in the server
+ssh -i "$keyfile" $user@$ipaddr "rm -r $appFolder"
 
-ssh -i "$keyfile" $user@$ipaddr "rm -r $appFolder/.next/"
+echo "Previous build Removed"
 
-# Copy the new build to the server 
-scp -r -i "$keyfile" .next/* $user@$ipaddr:$appFolder/.next/
-
-scp -r -i "$keyfile" package.json $user@$ipaddr:$appFolder/package.json
+echo "Coping new build"
+ssh -i "$keyfile" $user@$ipaddr "mkdir $appFolder"
+# Copy the standalone folder to the app folder in the server
+scp -r -i "$keyfile" .next/standalone/* $user@$ipaddr:$appFolder/
+ssh -i "$keyfile" $user@$ipaddr "mkdir $appFolder/.next"
+# Copy the .next files
+scp -r -i "$keyfile" .next/standalone/.next/* $user@$ipaddr:$appFolder/.next/
+# Copy the env files
+scp -r -i "$keyfile" .env $user@$ipaddr:$appFolder/.env
+#scp -r -i tc-test-key1.pem .env ec2-user@13.127.153.17:/home/ec2-user/gotask/.env
 echo "New build Copied"
 
-ssh -i "$keyfile" $user@$ipaddr "pm2 restart 'test-gotask-today'"
+ssh -i "$keyfile" $user@$ipaddr "pm2 restart dev-gotask-today"
+# echo "Service Restarted"
 
-echo "Service Restarted"
 
-
-# after running this script
 #ssh -i "tc-test-key1.pem" ec2-user@13.127.153.17
-#pm2 start npm --name "test-gotask-today " -- start
+
+#pm2 start npm --name dev-gotask-today -- start
+#For the first time only - pm2 start server.js --name dev-gotask-today
+
+#Copy the public asset alone
+#scp -r -i tc-test-key1.pem public ec2-user@13.127.153.17:/home/ec2-user/gotask/public
---
+++ apps/gotask_ui/dev-build.sh
@@ -0,0 +1,13 @@
+# docker build -t test-build .
+
+# Remove the previous build and create new build
+rm -rf .next
+
+# Create new build
+npm run lint
+npm run format 
+npm run build 
+cp -r public .next/standalone/
+cp .env.local .next/standalone
+cp -r .next/static .next/standalone/.next/
+

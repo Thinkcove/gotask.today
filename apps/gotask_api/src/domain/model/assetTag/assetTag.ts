3a74f80e174f4248ac3abd41df8d6fd9e26aab41
Supriya R <supriya@thinkcove.com>
2025-06-18 17:29:21 +0530

    sr/updated api response

--- apps/gotask_api/src/domain/model/assetTag/assetTag.ts
+++ apps/gotask_api/src/domain/model/assetTag/assetTag.ts
@@ -1,31 +1,18 @@
 import { Document, Schema, model } from "mongoose";
 import { v4 as uuidv4 } from "uuid";
-import { ASSET_TAGS } from "../../../constants/assetConstant";
 
 export interface IAssetTag extends Document {
   id: string;
-  empId?: string;
   userId: string;
   assetId: string;
-  actionType: (typeof ASSET_TAGS)[keyof typeof ASSET_TAGS];
-  erk?: string;
-  previouslyUsedBy?: string;
   active?: boolean;
 }
 
 const AssetTagSchema = new Schema<IAssetTag>(
   {
     id: { type: String, default: uuidv4, unique: true },
-    empId: { type: String },
     userId: { type: String, ref: "User", required: true },
     assetId: { type: String, ref: "Asset", required: true },
-    actionType: {
-      type: String,
-      enum: ASSET_TAGS,
-      required: true
-    },
-    erk: { type: String },
-    previouslyUsedBy: { type: String },
     active: { type: Boolean, default: true }
   },
   { timestamps: true }

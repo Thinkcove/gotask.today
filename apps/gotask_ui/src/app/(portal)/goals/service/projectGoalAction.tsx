3e31acea97d0ce05de2b271ac80920431ef12f75
Pavi <pavithra@thinkcove.com>
2025-06-18 18:56:53 +0530

    p/api weite

---
+++ apps/gotask_ui/src/app/(portal)/goals/service/projectGoalAction.tsx
@@ -0,0 +1,50 @@
+import env from "@/app/common/env";
+import { getData, postData, putData } from "@/app/common/utils/apiData";
+import { withAuth } from "@/app/common/utils/authToken";
+
+
+export const fetchWeeklyGoals = async () => {
+  return withAuth(async (token) => {
+    const url = `${env.API_BASE_URL}/weekly/goals`;
+    const { data } = await getData(url, token);
+    return data || [];
+  });
+};
+
+// Create Weekly Goal
+export const createWeeklyGoal = async (goalData: {
+  projectId: string;
+  goalTitle: string;
+  weekStart: string;
+  weekEnd: string;
+  status: string;
+  description: string;
+  comments: string[];
+  priority: string;
+}) => {
+  return withAuth(async (token) => {
+    const url = `${env.API_BASE_URL}/weekly/goals`;
+    return await postData(url, goalData, token);
+  });
+};
+
+
+export const updateWeeklyGoal = async (
+  goalId: string,
+  updatedGoalData: {
+    projectId: string;
+    goalTitle: string;
+    weekStart: string;
+    weekEnd: string;
+    status: string;
+    description: string;
+    comments: string[];
+    priority: string;
+  }
+) => {
+  return withAuth(async (token) => {
+    const url = `${env.API_BASE_URL}/weekly/goals/${goalId}`;
+    return await putData(url, updatedGoalData, token);
+  });
+};
+

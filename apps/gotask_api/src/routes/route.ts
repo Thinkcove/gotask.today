968f92226d41093ba2c89bca039d107ea6334f76
Pavi <pavithra@thinkcove.com>
2025-06-18 13:50:23 +0530

    p/api curd

--- apps/gotask_api/src/routes/route.ts
+++ apps/gotask_api/src/routes/route.ts
@@ -12,7 +12,7 @@ import * as QueryRoutes from "../modules/query/queryRoutes";
 import * as AttendanceRoutes from "../modules/attendance/attendanceRoutes";
 import * as QueryTaskRoutes from "../modules/queryTask/queryTaskRoutes";
 import ProjectStoryRoutes from "../modules/projectStory/projectStoryRoutes";
-
+import * as trackweklyRoute from "../modules/trackwekly/trackweklyRoute";
 import * as AssetRoutes from "../modules/assets/assetsRoutes";
 import * as AssetTagRoutes from "../modules/assetTag/assetTagRoutes";
 
@@ -31,6 +31,7 @@ const routes: ServerRoute[] = [
   ...UserPreferenceRoutes.default,
   ...ProjectStoryRoutes,
   ...AssetRoutes.default,
-  ...AssetTagRoutes.default
+  ...AssetTagRoutes.default,
+  ...trackweklyRoute.default
 ];
 export default routes;

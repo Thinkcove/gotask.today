e1d9ceec71871e01078443db36badd1a62c7ed98
Pavi <pavithra@thinkcove.com>
2025-06-20 12:26:20 +0530

    name changed

--- apps/gotask_ui/src/app/(portal)/goals/projectid/[projectId]/components/projectGoal/projectGoals.tsx
+++ apps/gotask_ui/src/app/(portal)/goals/projectid/[projectId]/components/projectGoal/projectGoals.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Grid, Box, Typography } from "@mui/material";
-import GoalCard from "@/app/(portal)/goals/projectid/[projectId]/components/projectGoal/goalCard";
 import { useTranslations } from "next-intl";
 import { LOCALIZATION } from "@/app/common/constants/localization";
 import { ProjectGoalsProps } from "@/app/(portal)/goals/projectid/[projectId]/interface/projectGoal";
+import GoalCard from "@/app/(portal)/goals/projectid/[projectId]/components/projectGoal/projectGoalCards";
 
 const ProjectGoals: React.FC<ProjectGoalsProps> = ({ projectGoals, projectId, handleEditGoal }) => {
   const filteredGoals = projectGoals.filter((goal) => goal.projectId === projectId);

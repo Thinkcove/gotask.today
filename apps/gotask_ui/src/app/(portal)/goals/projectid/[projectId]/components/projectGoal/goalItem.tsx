4d50a945a8d450f8f9a33e3c18dd9671dd67318b
Pavi <pavithra@thinkcove.com>
2025-06-20 17:40:50 +0530

    card hight fixed auto

--- apps/gotask_ui/src/app/(portal)/goals/projectid/[projectId]/components/projectGoal/goalItem.tsx
+++ apps/gotask_ui/src/app/(portal)/goals/projectid/[projectId]/components/projectGoal/goalItem.tsx
@@ -13,14 +13,14 @@ const GoalItem: React.FC<GoalCardProps> = ({ goal, onEdit, onClick }) => {
 
   return (
     <Box
-      onClick={onClick} 
+      onClick={onClick}
       sx={{
         p: 2,
         borderRadius: 2,
         backgroundColor: `${color}12`,
         border: `1px solid ${color}40`,
         transition: "all 0.2s ease-in-out",
-        cursor: onClick ? "pointer" : "default", 
+        cursor: onClick ? "pointer" : "default",
         display: "flex",
         flexDirection: "column",
         height: "100%",
@@ -37,34 +37,28 @@ const GoalItem: React.FC<GoalCardProps> = ({ goal, onEdit, onClick }) => {
         </Typography>
 
         <StatusIndicator status={goal.status} getColor={getStatusColor} />
-
-        {goal.status?.toLowerCase() === "completed" && (
-          <Box display="flex" alignItems="center" mt={1}>
-            <Typography variant="body2" sx={{ mr: 1 }}>
-              {transGoal("completion")}
-            </Typography>
-            <input type="checkbox" disabled checked />
+        <Box sx={{ display: "flex", alignItems:"center"}}>
+          <Typography variant="body2" sx={{  }}>
+            {transGoal("completion")}
+          </Typography>
+          <input type="checkbox" disabled checked />
+          <Box sx={{ ml: "auto" }}>
+            <Tooltip title="Edit Goal">
+              <IconButton
+                onClick={(e) => {
+                  e.stopPropagation();
+                  onEdit(goal);
+                }}
+                color="primary"
+              >
+                <Edit />
+              </IconButton>
+            </Tooltip>
           </Box>
-        )}
-      </Box>
-
-      {/* Edit Button */}
-      <Box display="flex" justifyContent="flex-end" mt={2}>
-        <Tooltip title="Edit Goal">
-          <IconButton
-            onClick={(e) => {
-              e.stopPropagation(); 
-              onEdit(goal);
-            }}
-            color="primary"
-          >
-            <Edit />
-          </IconButton>
-        </Tooltip>
+        </Box>
       </Box>
     </Box>
   );
 };
-
 
 export default GoalItem;

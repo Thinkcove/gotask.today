7cd8ea2d58fff2235802831e72b3304261db39d6
Pavi <pavithra@thinkcove.com>
2025-06-25 13:24:48 +0530

    name update

--- apps/gotask_api/src/domain/model/projectGoal/projectGoalUpdateHistory.ts
+++ apps/gotask_api/src/domain/model/projectGoal/projectGoalUpdateHistory.ts
@@ -7,14 +7,14 @@ export interface IProjectGoalUpdateHistory extends Document {
   id: string;
   goal_id: string;
   updated_by: string;
-  update_goal_historydata: Record<string, any>;
+  history_data: Record<string, any>;
 }
 
 const ProjectGoalUpdateHistorySchema = new Schema<IProjectGoalUpdateHistory>({
   id: { type: String, default: uuidv4, unique: true },
   goal_id: { type: String, required: true, ref: "ProjectGoal" },
   updated_by: { type: String, required: true }, // user_id
-  update_goal_historydata: { type: Schema.Types.Mixed, required: true }
+  history_data: { type: Schema.Types.Mixed, required: true }
 });
 
 export const ProjectGoalUpdateHistory = model<IProjectGoalUpdateHistory>(

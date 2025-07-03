577cd6e82857af44b98995764284cc36f2cdf919
snega <snegathinkcove.com>
2025-07-03 09:57:35 +0530

    routing back

--- apps/gotask_ui/src/app/(portal)/task/createTask/createTask.tsx
+++ apps/gotask_ui/src/app/(portal)/task/createTask/createTask.tsx
@@ -96,7 +96,7 @@ const CreateTask: React.FC = () => {
       if (storyId) {
         router.push(`/project/view/${formData.project_id}/stories/${storyId}`);
       } else {
-        router.push("/task/projects?refresh=true");
+        router.back();
       }
     } catch (error) {
       console.error("Error while creating task:", error);

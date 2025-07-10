69c3df88fb60a98bba6d2159321d8fbfaa268b11
snega <snegathinkcove.com>
2025-07-10 11:04:32 +0530

    file name changed

--- apps/gotask_ui/src/app/(portal)/report/components/logStyle.ts
+++ apps/gotask_ui/src/app/(portal)/report/components/logStyle.ts
@@ -1,6 +1,6 @@
 // src/app/common/constants/styles.ts
 
-import { DAILY_LOG_HOURS_LIMIT } from "@/app/common/constants/threshold";
+import { DAILY_LOG_HOURS_LIMIT } from "@/app/common/constants/report";
 
 export const getDailyLogCellStyle = (hours?: number) => {
   return hours && hours > DAILY_LOG_HOURS_LIMIT ? { color: "#ff0000" } : {};
--- apps/gotask_ui/src/app/common/constants/threshold.ts
+++
@@ -1,2 +0,0 @@
-// src/app/common/constants/thresholds.ts
-export const DAILY_LOG_HOURS_LIMIT = 10;

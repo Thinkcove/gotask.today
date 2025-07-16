4eca2846b1414bef63436ad5bf80c385ca9b4c51
SUma <uma@thinkcove.com>
2025-07-16 20:13:47 +0530

    Build error resolved

--- apps/gotask_ui/src/app/(portal)/project/createProject/page.tsx
+++ apps/gotask_ui/src/app/(portal)/project/createProject/page.tsx
@@ -3,7 +3,11 @@ import React from "react";
 import { LOCALIZATION } from "@/app/common/constants/localization";
 import { useTranslations } from "next-intl";
 import ModuleHeader from "@/app/component/header/moduleHeader";
-import CreateProject from "./createProject";
+import dynamic from "next/dynamic";
+
+const CreateProject = dynamic(() => import("./createProject"), {
+  ssr: false
+});
 
 const CreateProjectPage: React.FC = () => {
   const transproject = useTranslations(LOCALIZATION.TRANSITION.PROJECTS);

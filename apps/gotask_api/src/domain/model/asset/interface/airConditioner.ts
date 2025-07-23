ec782e21fc9c4aa13addbdb63d894e1c94493f57
Supriya R <supriya@thinkcove.com>
2025-07-23 17:06:23 +0530

    sr/ac interface updated

--- apps/gotask_api/src/domain/model/asset/interface/airConditioner.ts
+++ apps/gotask_api/src/domain/model/asset/interface/airConditioner.ts
@@ -1,10 +1,9 @@
 import { IAsset } from "./asset";
 
 export interface IAirConditionerAsset extends IAsset {
-  location?: string;
-  connectivity?: string;
-  capacity?: string;
-  authenticationModes?: string;
-  display?: string;
-  cloudAndAppBased?: boolean;
+  acType?: string;
+  energyRating?: string;
+  powerConsumption?: string;
+  coolingCoverage?: string;
+  inverterType?: string;
 }
